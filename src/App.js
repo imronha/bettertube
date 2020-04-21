4a139524c9984817c6e5f0c0474847a6103edeee
Imron Hajiahmad <imronha@gmail.com>
2020-04-21 13:50:30 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import youtube from "./api/youtube";
+
+import { SearchBar, VideoDetail, VideoList } from "./components";
+
+class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get("/search", {
+      params: {
+        part: "snippet",
+        key: "AIzaSyD75kGXfYRQmrpklu_ayL7Kxj00PUmRtO0",
+        q: searchTerm,
+      },
+    });
+    // console.log(response.data.items);
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  render() {
+    const { selectedVideo, videos } = this.state;
+    return (
+      <Grid justify="center" container spacing={8}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail video={selectedVideo} />
+            </Grid>
+            <Grid item xs={4}>
+              <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default App;
